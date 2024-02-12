d3d8ea7b43473651ba6ada1d4487e24dbb9c2c3a
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-02-12 16:16:03 +0000

    Practice tasks

---
+++ src/PB05WhileLoop/Lab/P04Sequence2kPlus1.java
@@ -0,0 +1,17 @@
+package PB05WhileLoop.Lab;
+
+import java.util.Scanner;
+
+public class P04Sequence2kPlus1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int currentNumber = 1;
+
+        while (currentNumber <= n) {
+            System.out.println(currentNumber);
+            currentNumber = currentNumber * 2 + 1;
+        }
+    }
+}
