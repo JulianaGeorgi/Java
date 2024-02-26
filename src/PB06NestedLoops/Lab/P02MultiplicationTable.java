ce15215b670c903f2979fc0b46ba667f5f9eb0c3
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-02-26 12:19:07 +0000

    Practice tasks

---
+++ src/PB06NestedLoops/Lab/P02MultiplicationTable.java
@@ -0,0 +1,15 @@
+package PB06NestedLoops.Lab;
+
+import java.util.Scanner;
+
+public class P02MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int num1 = 1; num1 <= 10; num1++) {
+            for (int num2 = 1; num2 <= 10; num2++) {
+                int res = num1 * num2;
+                System.out.printf("%d * %d = %d%n", num1, num2, res);
+            }
+        }
+    }
+}
