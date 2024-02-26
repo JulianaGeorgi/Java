26e778f19f093c5c8d9a5d0fd9402c0c77f9f13c
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-02-26 13:07:12 +0000

    Practice tasks

---
+++ src/PB06NestedLoops/Lab/P04SumOfTwoNumbers.java
@@ -0,0 +1,35 @@
+package PB06NestedLoops.Lab;
+
+import java.util.Scanner;
+
+public class P04SumOfTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startNum = Integer.parseInt(scanner.nextLine());
+        int endNum = Integer.parseInt(scanner.nextLine());
+        int magicNum = Integer.parseInt(scanner.nextLine());
+
+        int count = 0;
+        boolean flag = false;
+
+        for (int i = startNum; i <= endNum; i++) {
+            for (int j = startNum; j <= endNum; j++) {
+                count++;
+                int sum = i + j;
+                if (sum == magicNum) {
+                    System.out.printf("Combination N:%d (%d + %d = %d)", count, i, j, magicNum);
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
+
+        if(!flag){
+            System.out.printf("%d combinations - neither equals %d", count, magicNum);
+        }
+    }
+}
